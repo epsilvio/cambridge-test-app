e6f08249580be43c012f1fd0a1f6663f72b732ef
Elisha Silvio <elisha.silvio123@gmail.com>
2023-07-01 03:31:57 +0800

    used cors proxy service for api call

--- src/api/fetchQuestions.tsx
+++ src/api/fetchQuestions.tsx
@@ -1,25 +1,23 @@
-import * as $ from "jquery";
+// import * as $ from "jquery";
+import axios from "axios";
 
-interface APIdata {
-  name: string;
-  heading: string;
-  activities: string[];
-}
+// export const getDataFromJquery = (url: string): Promise<any> => {
+//   return new Promise((resolve, reject) => {
+//     $.ajaxPrefilter(function (options) {
+//       if (options.crossDomain && jQuery.support.cors) {
+//         options.url = "https://cors-anywhere.herokuapp.com/" + options.url;
+//       }
+//     });
+//   });
+// };
 
-const fetchData = (url: string): Promise<APIdata> => {
+export const getDataFromAxios = async (url: string): Promise<any> => {
   return new Promise((resolve, reject) => {
-    $.ajax({
-      url: url,
-      method: "GET",
-      dataType: "json",
-      success: (data: APIdata) => {
-        resolve(data);
-      },
-      error: (xhr, textStatus, errorThrown) => {
-        reject(new Error(textStatus));
-      },
-    });
+    axios
+      .get("https://cors-anywhere.herokuapp.com/" + url)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => reject(err));
   });
 };
-
-export default fetchData;
