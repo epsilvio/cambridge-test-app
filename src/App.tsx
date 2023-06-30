1a807623762c1434080668eaf74c17861574b5f8
Elisha Silvio <elisha.silvio123@gmail.com>
2023-07-01 03:30:26 +0800

    1st stable build

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,186 @@
-import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import fetchData from "./api/fetchQuestions";
+import { getDataFromAxios } from "./api/fetchQuestions";
 import { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/styles";
 import LoadingScreen from "./components/LoadingScreen";
+import ScoresModal from "./components/ScoresModal";
+import ActivityField from "./components/ActivityField";
+import {
+  Button,
+  Box,
+  AppBar,
+  CssBaseline,
+  Toolbar,
+  Typography,
+  Link,
+  Grid,
+  Stack,
+  Container,
+} from "@mui/material";
+
+type HomeScreenProps = {
+  name: string;
+  description: string;
+  activities: ActivityType[];
+};
+
+type GameScreenProps = {
+  activity: ActivityType;
+};
+
+interface APIdata {
+  name: string;
+  heading: string;
+  activities: ActivityType[];
+}
+
+interface ActivityType {
+  activity_name: string;
+  order: number;
+  questions: QuestionType[];
+}
+
+interface QuestionType {
+  round_title?: string;
+  questions?: Question[];
+  is_correct?: boolean;
+  stimulus?: string;
+  order?: number;
+  user_answers?: [];
+  feedback?: string;
+}
+
+interface Question {
+  is_correct: boolean;
+  stimulus: string;
+  order: number;
+  user_answers: boolean[];
+  feedback: string;
+}
+
+const HomeScreen: React.FC<HomeScreenProps> = ({
+  name,
+  description,
+  activities,
+}) => {
+  const [pageContent, setPageContent] = useState<string>("home");
+
+  useEffect(() => {
+    setPageContent("home");
+  }, []);
+
+  const HomeContent = () => (
+    <main>
+      {/* Hero unit */}
+      <Box
+        sx={{
+          pt: 28,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            {name}
+          </Typography>
+          <Typography
+            variant="h6"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            {description}
+          </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Button
+              variant="contained"
+              onClick={() => {
+                setPageContent("game1");
+              }}
+              sx={{ backgroundColor: "#a009db" }}
+            >
+              Activity One
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setPageContent("game2");
+              }}
+              sx={{ backgroundColor: "#a009db" }}
+            >
+              Activity Two
+            </Button>
+            <ScoresModal />
+          </Stack>
+        </Container>
+      </Box>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        {/* End hero unit */}
+        <Grid container spacing={4}>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </Grid>
+      </Container>
+    </main>
+  );
+
+  const GameContent: React.FC<GameScreenProps> = ({ activity }) => (
+    <main>
+      {/* Hero unit */}
+      <Box
+        sx={{
+          pt: 28,
+          pb: 6,
+        }}
+      >
+        <ActivityField
+          activity_name={activity.activity_name}
+          order={activity.order}
+          questions={activity.questions}
+        />
+      </Box>
+    </main>
+  );
+
+  return (
+    <>
+      {pageContent === "home" ? (
+        <HomeContent />
+      ) : pageContent === "game1" ? (
+        <GameContent
+          activity={
+            activities.filter(
+              (activity: ActivityType) => activity.order === 1
+            )[0]
+          }
+        />
+      ) : pageContent === "game2" ? (
+        <GameContent
+          activity={
+            activities.filter(
+              (activity: ActivityType) => activity.order === 2
+            )[0]
+          }
+        />
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
+  );
+};
 
 const Copyright = () => {
   return (
@@ -30,12 +198,6 @@ const Copyright = () => {
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
-interface APIdata {
-  name: string;
-  heading: string;
-  activities: string[];
-}
-
 const useStyles = makeStyles(() => ({
   logo: {
     marginRight: defaultTheme.spacing(2),
@@ -43,99 +205,53 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const HomeScreen = () => {
-  // Initialize state variables
+const App = () => {
+  // Initialize state variables and hooks
   const [apiData, setApiData] = useState<APIdata | null>();
   const [isReady, setReady] = useState<boolean>(false);
+  const classes = useStyles();
 
   useEffect(() => {
-    try {
-      fetchData("/api/payload.json").then((response) => {
-        setApiData(response);
-      });
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+    getDataFromAxios(
+      "https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json"
+    )
+      .then((val) => setApiData(val.data))
+      .catch((error) => alert(error));
   }, []);
 
   useEffect(() => {
-    apiData !== null ? setReady(true) : void 0;
+    apiData !== null && apiData !== undefined
+      ? setReady(true)
+      : setReady(false);
   }, [apiData]);
-
-  const classes = useStyles();
 
   return (
     <>
-      {!!isReady ? (
+      {!!isReady && apiData ? (
         <ThemeProvider theme={defaultTheme}>
           <CssBaseline />
-          <AppBar position="relative" sx={{ backgroundColor: "#7C7C7C" }}>
+          <AppBar position="relative" sx={{ backgroundColor: "#a009db" }}>
             <Toolbar>
               <img src="/CUPA-logo.png" alt="Logo" className={classes.logo} />
               <Typography variant="h6" color="inherit" noWrap></Typography>
             </Toolbar>
           </AppBar>
-          <main>
-            {/* Hero unit */}
-            <Box
-              sx={{
-                bgcolor: "background.paper",
-                pt: 28,
-                pb: 6,
-              }}
-            >
-              <Container maxWidth="sm">
-                <Typography
-                  component="h1"
-                  variant="h2"
-                  align="center"
-                  color="text.primary"
-                  gutterBottom
-                >
-                  {apiData?.name}
-                </Typography>
-                <Typography
-                  variant="h6"
-                  align="center"
-                  color="text.secondary"
-                  paragraph
-                >
-                  {apiData?.heading}
-                </Typography>
-                <Stack
-                  sx={{ pt: 4 }}
-                  direction="row"
-                  spacing={2}
-                  justifyContent="center"
-                >
-                  <Button
-                    variant="contained"
-                    sx={{ backgroundColor: "#7C7C7C" }}
-                  >
-                    Start Quiz!
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    sx={{ borderColor: "#7C7C7C", color: "#7C7C7C" }}
-                  >
-                    View My Scores
-                  </Button>
-                </Stack>
-              </Container>
-            </Box>
-            <Container sx={{ py: 8 }} maxWidth="md">
-              {/* End hero unit */}
-              <Grid container spacing={4}>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-              </Grid>
-            </Container>
-          </main>
+          <HomeScreen
+            name={apiData.name}
+            description={apiData.heading}
+            activities={apiData.activities}
+          />
           {/* Footer */}
-          <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+          <Box
+            textAlign="center"
+            position="fixed"
+            left={0}
+            bottom={0}
+            width="100%"
+            p={2}
+            sx={{ bgcolor: "background.paper", p: 6 }}
+            component="footer"
+          >
             <Typography variant="h6" align="center" gutterBottom>
               Error Find
             </Typography>
@@ -159,4 +275,4 @@ const HomeScreen = () => {
   );
 };
 
-export default HomeScreen;
+export default App;
