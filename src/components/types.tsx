0379c3d7688c38f960898ebaa853842d3bd882c0
Elisha Silvio <elisha.silvio123@gmail.com>
2023-07-04 17:46:30 +0800

    separate file for types and interface declaration

---
+++ src/components/types.tsx
@@ -0,0 +1,51 @@
+export type ResultModalProps = {
+  onConfirm: () => void;
+  showResult: boolean;
+  activity_name: string;
+  order: number;
+  questions: QuestionType[];
+};
+
+export type ActivityProps = {
+  activity_name: string;
+  order: number;
+  questions: QuestionType[];
+};
+
+export type FormattedStringProps = {
+  text: string;
+};
+
+export interface QuestionType {
+  round_title?: string;
+  questions?: Question[];
+  is_correct?: boolean;
+  stimulus?: string;
+  order?: number;
+  user_answers?: boolean[];
+  feedback?: string;
+}
+
+export interface Question {
+  is_correct: boolean;
+  stimulus: string;
+  order: number;
+  user_answers: boolean[];
+  feedback: string;
+}
+
+export type HomeScreenProps = {
+  name: string;
+  description: string;
+  activities: ActivityProps[];
+};
+
+export type GameScreenProps = {
+  activity: ActivityProps;
+};
+
+export interface APIdata {
+  name: string;
+  heading: string;
+  activities: ActivityProps[];
+}
